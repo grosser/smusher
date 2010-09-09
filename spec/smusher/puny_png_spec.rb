a160a33b0a0dea3944064d1ae260a23c78d43410
Nate Pickens <nate.pickens@plastictrophy.com>
2010-09-09 11:59:41 -0700

    update requires in spec files to work with ruby 1.9

--- spec/smusher/puny_png_spec.rb
+++ spec/smusher/puny_png_spec.rb
@@ -1,4 +1,4 @@
-require 'spec/spec_helper'
+require 'spec_helper'
 
 describe Smusher::PunyPng do
   describe :optimized_image_data_for do
--- spec/smusher/smush_it_spec.rb
+++ spec/smusher/smush_it_spec.rb
@@ -1,4 +1,4 @@
-require 'spec/spec_helper'
+require 'spec_helper'
 
 describe Smusher::SmushIt do
   describe :optimized_image_data_for do
