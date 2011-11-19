6d284a551d6ae23040622b3510511e8852f79161
grosser <grosser.michael@gmail.com>
2011-11-18 20:10:12 -0800

    make url overwriteable + found the new url + document how to find the new url

--- lib/smusher/smush_it.rb
+++ lib/smusher/smush_it.rb
@@ -1,17 +1,24 @@
 module Smusher
   class SmushIt
+    #I leave these urls here, just in case it stops working again...
+    # URL = "http://smush.it/ws.php" # original, redirects to somewhere else..
+    # URL = "http://developer.yahoo.com/yslow/smushit/ws.php" # official but does not work
+    # URL = "http://smushit.com/ysmush.it/ws.php" # used at the new page but does not hande uploads
+    # URL = "http://smushit.eperf.vip.ac4.yahoo.com/ysmush.it/ws.php" # used at the new page but does not hande uploads
+    # URL = 'http://ws1.adq.ac4.yahoo.com/ysmush.it/ws.php'
+    # URL = 'www.smushit.com/ysmush.it/ws.php'
+
+    # How to find a new url:
+    # go to the smusher page and look for 'files' in the js source
+    # find then url where the 'files' get send + add /ws.php
+    URL = 'http://ypoweb-01.experf.gq1.yahoo.com/ysmush.it/ws.php'
+
     def self.converts_gif_to_png?
       true
     end
 
     def self.optimized_image_data_for(file)
-      #I leave these urls here, just in case it stops working again...
-      # url = "http://smush.it/ws.php" # original, redirects to somewhere else..
-      # url = "http://developer.yahoo.com/yslow/smushit/ws.php" # official but does not work
-      # url = "http://smushit.com/ysmush.it/ws.php" # used at the new page but does not hande uploads
-      # url = "http://smushit.eperf.vip.ac4.yahoo.com/ysmush.it/ws.php" # used at the new page but does not hande uploads
-        url = 'http://ws1.adq.ac4.yahoo.com/ysmush.it/ws.php'
-      response = HTTPClient.post url, { 'files[]' => File.new(file)}
+      response = HTTPClient.post URL, 'files[]' => File.new(file)
       response = JSON.parse(response.body)
       raise "smush.it: #{response['error']}" if response['error']
       image_url = response['dest']
